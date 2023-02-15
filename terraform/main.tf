de8d601c44bfb40a1ac79865564863eab7bc1b6c
Cristian Larios <cflarios@correo.iue.edu.co>
2023-02-15 17:41:34 -0500

    terraform load balancer fix and remote exec

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,13 @@ resource "digitalocean_loadbalancer" "public" {
     target_protocol = "http"
   }
 
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+    check_interval_seconds = 10
+    response_timeout_seconds = 10
+  }
+
   droplet_ids = digitalocean_droplet.example.*.id
 }
 
@@ -20,6 +27,21 @@ resource "digitalocean_droplet" "example" {
   name   = "devsu-${count.index+1}"
   region = "nyc1"
   size   = "s-1vcpu-1gb"
-  user_data = "${file("userdata.yaml")}"
   ssh_keys = ["${digitalocean_ssh_key.cflarios.fingerprint}"]
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.ipv4_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1 ; do sleep 1 ; done",
+      "sudo apt install -y docker.io",
+      "sudo docker run -d -p 80:3001 cflarios/devsu_devops_assessment:latest"
+    ]
+  }
 }

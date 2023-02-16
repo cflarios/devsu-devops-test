43c34db2ad65384631af8aa70d39902465e8bcd2
Cristian Larios <cflarios@correo.iue.edu.co>
2023-02-15 19:19:37 -0500

    added new tests

--- test/app.test.js
+++ test/app.test.js
@@ -11,3 +11,30 @@ describe('POST /DevOps', () => {
         expect(response.statusCode).toBe(200);
     });
 });
+
+// Test if the jwt key is correct
+test('should return 404 NOT FOUND', async () => {
+    const response = await request(app).post('/DevOps')
+        .send({ message: 'This is a Test', to: 'John', from: 'Jane', timeToLifeSec: 10 })
+        .set('X-Parse-REST-API-Key', process.env.API_KEY)
+        .set('X-JWT-KWY', 'wrong');
+    expect(response.statusCode).toBe(404);
+});
+
+// Test if the api key is correct
+test('should return 404 NOT FOUND', async () => {
+    const response = await request(app).post('/DevOps')
+        .send({ message: 'This is a Test', to: 'John', from: 'Jane', timeToLifeSec: 10 })
+        .set('X-Parse-REST-API-Key', 'wrong')
+        .set('X-JWT-KWY', process.env.JWT_SECRET);
+    expect(response.statusCode).toBe(404);
+});
+
+// Test if the message is correct
+test('should return the correct message', async () => {
+    const response = await request(app).post('/DevOps')
+        .send({ message: 'This is a Test', to: 'John', from: 'Jane', timeToLifeSec: 10 })
+        .set('X-Parse-REST-API-Key', process.env.API_KEY)
+        .set('X-JWT-KWY', process.env.JWT_SECRET);
+    expect(response.body.message).toBe('Hello John your message will be send');
+});
